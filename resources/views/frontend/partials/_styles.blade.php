110ae5323e5826a26ec2704ba25ba07fda7964f8
unknown <alauddinnajim202@gmail.com>
2026-09-15 12:21:27 +0600

    Add premium mobile responsive design for frontend

--- resources/views/frontend/partials/_styles.blade.php
+++ resources/views/frontend/partials/_styles.blade.php
@@ -15,6 +15,9 @@
 
 <!-- Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Barlow:200,300,300i,400,400i,500,500i,600,700,800" rel="stylesheet">
-<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,600italic,700,700italic,800' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+<!-- Custom Frontend Styling & Mobile Enhancements -->
+<link rel="stylesheet" href="{{ asset('assets/css/custom-frontend.css') }}">
